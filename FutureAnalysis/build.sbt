81686e5f4e6393e7c9f1c2c40025a439b5593119
CMBlair <corey514@revature.net>
2022-08-04 15:02:30 -0400

    Creates Spark Session
    Creates Spark Session

---
+++ FutureAnalysis/build.sbt
@@ -0,0 +1,11 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.8"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "FutureAnalysis"
+  )
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.2.1"
---
+++ FutureAnalysis/src/main/scala/FutureTest.scala
@@ -0,0 +1,14 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+object FutureTest {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("PokemonMoves")
+      .config("spark.master", "local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    println("created spark session")
+  }
+}
