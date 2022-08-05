4ea76c1df95b2b9c20bc133f0624fa5b87ec07df
Edwin <escore99@gmail.com>
2022-08-05 16:57:16 -0400

    some changes output format and csv files added to the root project

--- FutureAnalysis/src/main/scala/FutureTest.scala
+++ FutureAnalysis/src/main/scala/FutureTest.scala
@@ -1,7 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, SparkSession}
- import scala.collection.convert.ImplicitConversions.{`list asScalaBuffer`, `map AsJavaMap`}
+import scala.collection.convert.ImplicitConversions.{`list asScalaBuffer`, `map AsJavaMap`}
 
 
 object FutureTest {
@@ -14,66 +14,65 @@ object FutureTest {
       .getOrCreate()
     Logger.getLogger("org").setLevel(Level.ERROR)
     println("created spark session")
-    def projection(stateCode: String, year1: Long, year2: Long, year3: Long): Array[Array[Long]]= {
-      var years = Array(
-        Array(2000, year1),
-        Array(2010, year2),
-        Array(2020, year3)
-      )
 
-      for(i <- 1 to 3) {
-        var year1 :Double  = years(years.size - 3)(1)
-        var year2 :Double = years(years.size - 2)(1)
-        var year3 :Double = years(years.size - 1)(1)
-        var growth1 = ((year2 - year1 )/ year1)
+    val data2000 = spark.read.option("header", "true").option("inferSchema",
+      "true").format("csv").load(getClass.getResource("/Combine2000RG.csv").getPath)
 
-        println(stateCode + " population in 2000 : "+ year1 )
-        println(stateCode + " population in 2010 : "+ year2 )
-        println(stateCode + " population in 2020 : "+ year3 )
-        println("Growth 1 is  : "+ growth1)
-        var growth2 = ((year3 - year2) / year2)
-        println("Growth 2 is  : "+ growth2)
-        var derivative = (growth1 - growth2)  // negative downtrends :3c
-        println("Derivation is : " + derivative)
-        var growthDecay = 1- (derivative / growth1)
-        var year = 2020 + (i * 10)
-        println("Predicted Year is :" +year)
-        var population =(years.last(1) * (1 + (growth2 * growthDecay))).toLong
-        println("Future Population of " + stateCode + " in " + year+ "  :" + population)
-        println()
+    val data2010 = spark.read.option("header", "true").option("inferSchema",
+      "true").format("csv").load(getClass.getResource(
+      "/Combine2010RG.csv").getPath)
+    val data2020 = spark.read.option("header", "true").option("inferSchema",
+      "true").format("csv").load(getClass.getResource(
+      "/combine2020RG.csv").getPath)
 
-        years = years :+ Array(year, population)
-      }
-      years
-    }
-    var data = spark.read.option("header", "true").option("inferSchema",
-      "true").format("csv").load(
-      "Cleaned/Combine2000RG.csv")
-
-    var data2010 = spark.read.option("header", "true").option("inferSchema",
-      "true").format("csv").load(
-      "Cleaned/Combine2010RG.csv")
-    var data2020 = spark.read.option("header", "true").option("inferSchema",
-      "true").format("csv").load(
-      "Cleaned/combine2020RG.csv")
-
-    data.createOrReplaceTempView("Census2000")
+    data2000.createOrReplaceTempView("Census2000")
     data2010.createOrReplaceTempView("Census2010")
     data2020.createOrReplaceTempView("Census2020")
 
-    var df =spark.sql(" SELECT DISTINCT(f.STUSAB) , s.P0010001  , f.P0010001  , t.P0010001" +
-      " FROM Census2000  f INNER JOIN Census2010 s ON f.STUSAB =s.STUSAB INNER JOIN Census2020 t ON s.STUSAB=t.STUSAB Order By f.STUSAB  ").toDF("STUSAB", "Pop2010", "Pop2000", "Pop2020")
+    val df =spark.sql(" SELECT DISTINCT(f.STUSAB) , f.P0010001  , s.P0010001  ,  t.P0010001" +
+      " FROM Census2000  f INNER JOIN Census2010 s ON f.STUSAB =s.STUSAB INNER JOIN Census2020 t ON s.STUSAB=t.STUSAB Order By f.STUSAB  ").toDF("STUSAB", "Pop2000", "Pop2010", "Pop2020")
     df.show(53)
-    var list2010=df.select("Pop2010").collectAsList()
-    var list2000=df.select("Pop2000").collectAsList()
-    var list2020=df.select("Pop2020").collectAsList()
-    var statecode=df.select("STUSAB").collectAsList()
+    val statecode=df.select("STUSAB").collectAsList()
+    val list2000=df.select("Pop2000").collectAsList()
+    val list2010=df.select("Pop2010").collectAsList()
+    val list2020=df.select("Pop2020").collectAsList()
 
 
-      for(i <- 0 to list2020.length-1) {
-        println(projection(statecode(i)(0).toString, list2000(i)(0).toString.toLong, list2010(i)(0).toString.toLong, list2020(i)(0).toString.toLong))
+    for(i <- 0 to statecode.length-1) {
+
+      projection(statecode(i)(0).toString, list2000(i)(0).toString.toLong, list2010(i)(0).toString.toLong, list2020(i)(0).toString.toLong)
     }
+  }
 
+  def projection(stateCode: String, year1: Long, year2: Long, year3: Long): Unit= {
+    var years = Array(
+      Array(2000, year1),
+      Array(2010, year2),
+      Array(2020, year3)
+    )
 
+    for(i <- 1 to 3) {
+      val year1 :Double  = years(years.size - 3)(1)
+      val year2 :Double = years(years.size - 2)(1)
+      val year3 :Double = years(years.size - 1)(1)
+      val growth1 = ((year2 - year1 )/ year1)
+
+      println(stateCode + s" population in ${years(years.size - 3)(0)}: "+ year1 )
+      println(stateCode + s" population in ${years(years.size - 2)(0)}: "+ year2 )
+      println(stateCode + s" population in ${years(years.size - 1)(0)}:  "+ year3 )
+      println("Growth 1 is : "+ growth1)
+      val growth2 = ((year3 - year2) / year2)
+      println("Growth 2 is : "+ growth2)
+      val derivative = (growth1 - growth2)  // negative downtrends :3c
+      println("Derivation is: " + derivative)
+      val growthDecay = 1- (derivative / growth1)
+      val year = 2020 + (i * 10)
+      println("Predicted Year is:" +year)
+      val population =(years.last(1) * (1 + (growth2 * growthDecay))).toLong
+      println("Future Population of " + stateCode + " in " + year+ ": " + population)
+      println()
+
+      years = years :+ Array(year, population)
+    }
   }
 }
