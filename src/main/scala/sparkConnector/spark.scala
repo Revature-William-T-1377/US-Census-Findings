0f57bbe0924c9b64dd1ed4148505f5565642f7eb
Alex Sutherland <46937009+aalleexxss@users.noreply.github.com>
2022-08-18 07:30:40 -0600

    deleted commented code

--- src/main/scala/sparkConnector/spark.scala
+++ src/main/scala/sparkConnector/spark.scala
@@ -13,16 +13,6 @@ class spark (){
 
   var accessKey = "dsdsd"
   var secretKey = "dsdsds"
-  //val bufferedSource: BufferedSource = Source.fromFile("C:\\Resources\\rootkeyP3.csv")
-  var count = 0
-  /*for (line <- bufferedSource.getLines) {
-    val Array(val1, value) = line.split("=").map(_.trim)
-    count match {
-      case 0 => accessKey = value
-      case 1 => secretKey = value
-    }
-    count = count + 1
-  }*/
 
   System.setProperty("hadoop.home.dir", "C:\\hadoop3")
   val spark: SparkSession = SparkSession.builder()
